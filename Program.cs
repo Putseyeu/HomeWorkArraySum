aaf753de17c5d3d50095f6e8d992d8dcbe96a6ba
Putseyeu <putseyeu.a.d@gmail.com>
2021-12-20 20:24:01 +0100

    step2

--- Program.cs
+++ Program.cs
@@ -10,26 +10,30 @@ namespace HomeWorkArrayRowColumn
     {        
         static void Main(string[] args)
         {
-            int[,] array = new int[2, 4] { { 1, 3, 5, 7 }, { 2, 4, 6, 8 } };
-            int sumTwoLine = 0;
-            int multiplicationСolumn;
+            int[,] array = new int[2, 4] { { 10, 3, 5, 7 }, { 2, 4, 6, 8 } };
+            int sumSecondLine = 0;
+            int multiplicationСolumn = 1;
 
             for (int i = 0; i < array.GetLength(1); i++)
             {
-                sumTwoLine += array[1, i];
+                sumSecondLine += array[1, i];
             }
 
             for(int i = 0; i < array.GetLength(0); i++)
             {
-                for(int j =0; j < array.GetLength(1); j++)
+                for(int j = 0; j < array.GetLength(1); j++)
                 {
                     Console.Write(array[i, j] + " ");
                 }
                 Console.WriteLine();
             }
-            Console.WriteLine("Cумма второй строки массива равна: " + sumTwoLine);
+            Console.WriteLine("Cумма второй строки массива равна: " + sumSecondLine);            
 
-            multiplicationСolumn = array[0, 0] * array[1, 0];
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                multiplicationСolumn *= array[i, 0];
+            }
+            
             Console.WriteLine("Произведение первого столбца равно: " + multiplicationСolumn);
         }
     }
